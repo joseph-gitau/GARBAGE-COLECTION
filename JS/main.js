13766c5284b6653325b84160699cbe77514be418
hamisi <kilumohamisi@gmail.com>
2023-02-05 22:41:35 +0300

    added rating function

--- JS/main.js
+++ JS/main.js
@@ -2,27 +2,35 @@
 const aboutLink = document.getElementById("about-link");
 const driverLink = document.getElementById("driver-link");
 const requestLink = document.getElementById("request-link");
+const ratingLink = document.getElementById("rating");
 // modal selections
 const aboutModal = document.getElementById("about-modal");
 const loginModal = document.getElementById("login-modal");
 const requestModal = document.getElementById("request-modal");
+const ratingModal = document.getElementById("rating-modal");
 // close the modalselection
 const closeButtonAbout = document.querySelector(".about-close-button");
 const closeButtonDriver = document.querySelector(".driver-close-button");
 const closeButtonRequest = document.querySelector(".request-close-button");
+const closeButtonRating = document.querySelector(".rating-close-button");
 
 // open the modal when the about button is clicked
 aboutLink.addEventListener("click", function () {
   aboutModal.style.display = "block";
 });
-// open the modal when the request button is clicked
-requestLink.addEventListener("click", function () {
-  requestModal.style.display = "block";
-  // open the modal when the driver button is clicked
-});
+// open the modal when the driver button is clicked
 driverLink.addEventListener("click", function () {
   loginModal.style.display = "block";
 });
+// open the modal when the request button is clicked
+requestLink.addEventListener("click", function () {
+  requestModal.style.display = "block";
+});
+// open the modal when the rating button is clicked
+ratingLink.addEventListener("click", function () {
+  ratingModal.style.display = "block";
+});
+
 // close the modal when the close button is clicked for all modals
 closeButtonAbout.addEventListener("click", function () {
   aboutModal.style.display = "none";
@@ -33,16 +41,21 @@ closeButtonDriver.addEventListener("click", function () {
 closeButtonRequest.addEventListener("click", function () {
   requestModal.style.display = "none";
 });
+closeButtonRating.addEventListener("click", function () {
+  ratingModal.style.display = "none";
+});
 
 // close the modal when the user clicks outside the modal
 window.addEventListener("click", function (event) {
   if (
     event.target === loginModal ||
     event.target === requestModal ||
-    event.target === aboutModal
+    event.target === aboutModal ||
+    event.target === ratingModal
   ) {
     aboutModal.style.display = "none";
     loginModal.style.display = "none";
     requestModal.style.display = "none";
+    ratingModal.style.display = "none";
   }
 });
